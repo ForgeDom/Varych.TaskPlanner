41d42a4230857d1a362b439ca61b0dbe5d0f454d
Max <maxstill2006@gmail.com>
2025-10-15 20:24:51 +0300

    +

--- Varych.TaskPlanner.Domain.Logic1/SimpleTaskPlanner.cs
+++ Varych.TaskPlanner.Domain.Logic1/SimpleTaskPlanner.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Varych.TaskPlanner.DataAccess.Abstractions;   // IWorkItemsRepository
 using Varych.TaskPlanner.Domain.Models1;
 
 namespace Varych.TaskPlanner.Domain.Logic1
 {
     public class SimpleTaskPlanner
     {
-        public WorkItem[] CreatePlan(WorkItem[] items)
+        private readonly IWorkItemsRepository _repo;
+
+        public SimpleTaskPlanner(IWorkItemsRepository repo)
         {
-            List<WorkItem> workItems = items.ToList();
-            workItems.Sort((x, y) =>
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+        }
+
+        public WorkItem[] CreatePlan()
+        {
+            var items = _repo.GetAll() ?? Array.Empty<WorkItem>();
+            var list = new List<WorkItem>(items);
+
+            list.Sort((x, y) =>
             {
                 int result = y.Priority.CompareTo(x.Priority);
+
                 if (result == 0)
                 {
-                    result = x.DueDate.CompareTo(y.DueDate);
+                    var xDue = x.DueDate;
+                    var yDue = y.DueDate;
+
+                    result = xDue.CompareTo(yDue);
                 }
+
                 if (result == 0)
-                {
                     result = string.Compare(x.Title, y.Title, StringComparison.OrdinalIgnoreCase);
-                }
+
                 return result;
             });
 
-            return workItems.ToArray();
+            return list.ToArray();
         }
-
     }
 }
